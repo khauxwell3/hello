a433ad49a353813ed1c0cbc828ce96179d3221dc
khauxwell3 <46767828+khauxwell3@users.noreply.github.com>
2019-01-17 08:00:24 -0600

    Initial commit

---
+++ hello.c
@@ -0,0 +1,16 @@
+/**
+ * Author: Kylee Hauxwell
+ * Date: 2019/01/15
+ *
+ * A simple hello world program in C
+ *
+ */
+#include<stdlib.h>
+#include<stdio.h>
+
+int main(int argc, char **argv) {
+
+  printf("Kylee Hauxwell\n");
+
+  return 0;
+}
